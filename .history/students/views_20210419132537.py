53405746d8762367cf58392491f168cdeea0d7d1
saif <mohamedhawas123@gmail.com>
2021-04-19 15:53:19 +0200

    add video embd and list course and detail for students

---
+++ .history/students/views_20210419132537.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login
+from django.views.generic.edit import FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import CourseEnrollForm
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from courses.models import Course
+
+
+
+class StudentRegisterView(CreateView):
+    template_name = 'students/student/registeration.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('student_course_list')
+
+    def form_valid(self, form):
+        result= super().form_valid(form)
+        cd = form.cleaned_data
+        user = authenticate(username=cd['username'], password=cd['password1'])
+        login(self.request, user)
+        return result
+
+
+class StudentEnrollCourseView(LoginRequiredMixin, FormView):
+    course = None
+    form_class = CourseEnrollForm
+
+    def form_valid(self, form):
+        self.course = form.cleaned_data['course']
+        self.course.students.add(self.request.user)
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse_lazy('student_course_detail', args=[self.course.id])
+
+
+class StudentCourseListView(LoginRequiredMixin, ListView):
+    model = Course
+    template_name = 'students.course/list.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+
+
+class Student_Course_Detail(DetailView):
+    model = Course
+    template_name = 'students/course/detail.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
--- educa/static_root/static_root/settings.py
+++ educa/static_root/static_root/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'courses'
+    'courses',
+    'students'
 ]
 
 MIDDLEWARE = [
@@ -119,11 +120,24 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
+
+
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'educa')
+    os.path.join(BASE_DIR, 'educa'),
+    os.path.join(BASE_DIR, 'media')
 ]
 
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'educa/static_root')
+
+MEDIA_URl = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+from django.urls import reverse_lazy
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
